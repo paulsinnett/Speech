48b2443688439e551c6814dc62035749d541ad65
Paul Sinnett <paul.sinnett@gmail.com>
2015-12-07 12:04:10 +0000

    Simple example project
    click to box to show in game speech in a caption

---
+++ Assets/SayHello.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class SayHello : MonoBehaviour
+{
+    public Text text;
+
+    void OnMouseDown()
+    {
+        text.text = "Hello";
+    }
+}
